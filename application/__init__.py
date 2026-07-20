ac3c619cf1db8231a15b8a900f24e151a2c08c05
newbwxp <wuxp7535@gmail.com>
2026-07-20 20:27:04 +0800

    fix: monkey-patch pymongo to strip background from _id indexes

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,25 @@ import flask.json
 import json as _json
 flask.json.JSONEncoder = _json.JSONEncoder
 
+# Monkey-patch: MongoEngine sends 'background' on _id index,
+# MongoDB Atlas 7.0+ rejects any 'background' field on _id indexes (code 197)
+from pymongo.collection import Collection
+
+_original_create_indexes = Collection.create_indexes
+
+
+def _patched_create_indexes(self, indexes, **kwargs):
+    """Strip 'background' from _id index specs to avoid Atlas code 197."""
+    fixed = []
+    for idx in indexes:
+        if isinstance(idx, dict) and idx.get("key", {}).get("_id") is not None:
+            idx = {k: v for k, v in idx.items() if k != "background"}
+        fixed.append(idx)
+    return _original_create_indexes(self, fixed, **kwargs)
+
+
+Collection.create_indexes = _patched_create_indexes
+
 import configs
 from . import models
 from . import services
