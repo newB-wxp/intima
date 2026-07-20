fb77800b83b30d3b457985effd1bf18157577f84
newbwxp <wuxp7535@gmail.com>
2026-07-20 16:25:42 +0800

    fix: monkey-patch flask.json.JSONEncoder for flask-mongoengine Flask 3.x compat

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# Monkey-patch: flask-mongoengine requires flask.json.JSONEncoder removed in Flask 3.x
+import flask.json
+import json as _json
+flask.json.JSONEncoder = _json.JSONEncoder
+
 import configs
 from . import models
 from . import services
