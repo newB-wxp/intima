c1bedfefd2dce3b74b26b688fbf89ef77374984d
newbwxp <wuxp7535@gmail.com>
2026-07-20 20:45:59 +0800

    fix: also patch create_index (singular) in addition to create_indexes

--- application/__init__.py
+++ application/__init__.py
@@ -9,19 +9,33 @@ flask.json.JSONEncoder = _json.JSONEncoder
 # MongoDB Atlas 7.0+ rejects any 'background' field on _id indexes (code 197)
 from pymongo.collection import Collection
 
+_original_create_index = Collection.create_index
 _original_create_indexes = Collection.create_indexes
 
 
+def _strip_background_from_id(index_or_indexes):
+    """Remove 'background' from _id index specs (both single dict and list)."""
+    if isinstance(index_or_indexes, list):
+        return [
+            {k: v for k, v in idx.items() if k != "background"}
+            if isinstance(idx, dict) and idx.get("key", {}).get("_id") is not None
+            else idx
+            for idx in index_or_indexes
+        ]
+    if isinstance(index_or_indexes, dict) and index_or_indexes.get("key", {}).get("_id") is not None:
+        return {k: v for k, v in index_or_indexes.items() if k != "background"}
+    return index_or_indexes
+
+
+def _patched_create_index(self, keys, **kwargs):
+    return _original_create_index(self, _strip_background_from_id(keys), **kwargs)
+
+
 def _patched_create_indexes(self, indexes, **kwargs):
-    """Strip 'background' from _id index specs to avoid Atlas code 197."""
-    fixed = []
-    for idx in indexes:
-        if isinstance(idx, dict) and idx.get("key", {}).get("_id") is not None:
-            idx = {k: v for k, v in idx.items() if k != "background"}
-        fixed.append(idx)
-    return _original_create_indexes(self, fixed, **kwargs)
+    return _original_create_indexes(self, _strip_background_from_id(indexes), **kwargs)
 
 
+Collection.create_index = _patched_create_index
 Collection.create_indexes = _patched_create_indexes
 
 import configs
